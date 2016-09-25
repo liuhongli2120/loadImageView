6cde1e0a60ca622ef973aa8c566c59b3a5d4b4c5
刘宏立 <liuhongli2120@163.com>
2016-09-25 22:19:40 +0800

    准备工作

---
+++ loadImageView/loadImageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  loadImageView
+//
+//  Created by 刘宏立 on 16/9/25.
+//  Copyright © 2016年 lhl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ loadImageView/loadImageView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  loadImageView
+//
+//  Created by 刘宏立 on 16/9/25.
+//  Copyright © 2016年 lhl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
